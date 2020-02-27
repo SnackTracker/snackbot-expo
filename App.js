dafeca75e7d16e19966ad07996d2bf42d3c47b0c
Micky Shaked <mshaked@dotdash.com>
2020-02-27 17:22:50 -0500

    scans once until pushing button at bottom of screen

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
-  const scanned = false;
+  const [scanned, setScanned] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -14,9 +14,17 @@ export default function App() {
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
-    scanned = true;
+    console.log('a', scanned);
+    setScanned(true);
+    console.log('b', scanned);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
   };
+
+  const revertScan = () => {
+    console.log('revert');
+    scanned = false;
+  }
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -39,7 +47,14 @@ export default function App() {
         style={StyleSheet.absoluteFillObject}
       />
 
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => {return scanned = false;}} />}
+      {scanned && <Button
+        style={{
+          position: 'absolute',
+          bottom: '-5rem'
+          }}
+          title={'Tap to Scan Again' }
+          onPress={() => setScanned(false)}
+        />}
     </View>
   );
 }
